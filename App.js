fdd661aca94ad36cd54ad0bf741692fae5c26c04
Iury Sousa <Iiury017@gmail.com>
2023-09-17 01:26:49 -0300

    update

--- App.js
+++ App.js
@@ -1,13 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [tela, setTela] = useState("menu")
+  const [jogadorAtual, setJogadorAtual] = useState("")
+  const [tabuleiro, setTabuleiro] = useState([])
+  const [jogadasRestantes, setJogadasRestantes] = useState(0)
+  const [ganhador, setGanhador] = useState("")
+
+  function iniciarJogo(jogador) {
+    setJogadorAtual(jogador);
+    setJogadasRestantes(9);
+    setTabuleiro([
+      ["","",""],
+      ["","",""],
+      ["","",""]
+    ]);
+    setTela("jogo");
+  }
+  switch(tela){
+    case "menu":
+      return getTelaMenu();
+    case "jogo":
+      return getTelaJogo();
+    case "ganhador":
+      return getTelaGanhador();
+  }
+
+  function getTelaMenu(){
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text style={styles.titulo}>Jogo da Velha</Text>
+        <Text style={styles.subtitulo}>Selecione o primeiro jogador</Text>
+
+        <View style={styles.inlineItems}>
+          <TouchableOpacity style={styles.boxJogador} onPress={()=> iniciarJogo("X")}>
+            <Text style={styles.jogadorX}>X</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.boxJogador} onPress={()=> iniciarJogo("O")}>
+            <Text style={styles.jogadorO}>O</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    )
+  }
+  function getTelaJogo(){
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text style={styles.titulo}>Jogo da Velha</Text>
+        {
+          tabuleiro.map((linha,numeroLinha)=>{
+            return (
+              <View key={numeroLinha} style={styles.inlineItems}>
+                {
+                  linha.map((coluna,numeroColuna)=>{
+                    return (
+                      <TouchableOpacity 
+                      key={numeroColuna}
+                      style={styles.boxJogador} 
+                      onPress={()=> iniciarJogo("X")}>
+                        <Text style={ coluna === "X" ? styles.jogadorX : styles.jogadorO}>{coluna}</Text>
+                      </TouchableOpacity>
+                    )
+                  })
+                }
+              </View>
+            )
+          })
+        }
+      </View>
+    )
+  }
+  function getTelaGanhador(){
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text>GANHADOR</Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +92,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#333'
+  },
+  subtitulo: {
+    fontSize: 20,
+    color: '#555',
+    marginTop: 20
+  },
+  boxJogador: {
+    width: 80,
+    height: 80,
+    backgroundColor: '#ddd',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 5
+  },
+  jogadorX: {
+    fontSize: 60,
+    color: '#553fda'
+  },
+  jogadorO: {
+    fontSize: 60,
+    color: '#da3f3f'
+  },
+  inlineItems: {
+    flexDirection: 'row'
+  }
 });
